917efbd1d0cf99eb7a71542a2f1654651f5744ad
sona-joseph7 <sonajosephsj07@gmail.com>
2025-01-06 14:00:56 +0530

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style={{height:'250px',marginTop:'100px'}} className='mt-5 w-full bg-violet-600 text-white p-4'>
+      <div className='flex justify-between p-4'>
+        <div style={{width:'400px'}} className='intro'>
+          <h5 className='text-xl font-bold'><i className='fa-solid fa-truck-fast me-2'></i>E Cart</h5>
+          <p>Designed and built with all the love in the world by the Luminar team with the help of our contributors.</p>
+          <p>Code licensed Luminar, docs CC By 3.0</p>
+          <p>Currently v5.3.2.</p>
+        </div>
+        <div className='flex flex-col'>
+          <h5 className='text-xl font-bold'>Links</h5>
+          <Link to={'/'} style={{textDecoration:'none', color:'white'}} >Landing Page</Link>
+          <Link to={'/home'} style={{textDecoration:'none', color:'white'}} >Home Page</Link>
+          <Link to={'/history'} style={{textDecoration:'none', color:'white'}} >History Page</Link>
+        </div>
+
+        <div  className='flex flex-col'>
+        <h5 className='text-xl font-bold'>Guides</h5>
+        <a href="https://react.dev/" style={{textDecoration:'none',color:'white'}} target='_blank'>React</a>
+        <a href="https://react-bootstrap.github.io/" style={{textDecoration:'none',color:'white'}} target='_blank'>React Bootstrap</a>
+        <a href="https://reactrouter.com/" style={{textDecoration:'none',color:'white'}} target='_blank'>React Router</a>
+        </div>
+
+        <div  className='flex flex-col'>
+          <h5 className= 'text-xl font-bold'>Contacts</h5>
+          <div className="flex">
+            <input type="text" placeholder='Enter Your Email Here...' className='rounded p-1'/>
+            <button className='btn btn-info ms-2'><i className="fa-solid fa-arrow-right"></i></button>
+          </div>
+          <div className="icons flex justify-between mt-3">
+            <a href="https://x.com/?lang=en-in"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-brands fa-twitter"></i></a>
+
+            <a href="https://www.instagram.com/"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-brands fa-instagram"></i></a>
+
+            <a href="https://www.facebook.com/"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-brands fa-facebook"></i></a>
+
+            <a href="https://www.linkedin.com/"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-brands fa-linkedin"></i></a>
+
+            <a href="https://github.com/"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-brands fa-github"></i></a>
+
+            <a href="https://github.com/"style={{textDecoration:'none',color:'white'}} target='_blank'><i className="fa-solid fa-phone"></i></a>
+
+          </div>
+        </div>
+
+      </div>
+      <p className='text-center mt-3'>Copyright &copy; May 2024 Batch, Media Player. Build with React.</p>
+
+    </div>
   )
 }
 
